f06012384f08c59c66f4fd02e058531a22db64ac
Sriram <sriramachandra2387.k@gmail.com>
2023-10-24 18:26:01 +0530

    detail changs

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,11 +9,33 @@ import { TodoServiceService } from '../service/todo-service.service';
 })
 export class TodoComponent implements OnInit {
 
+  userNameArray = [
+    'Sachin Tendulkar',
+    'Saurabh Ganguly',
+    'Virendar Sehwag',
+    'Rahul Dravid',
+    'Yuvraj Singh',
+    'Mohammad Kaif',
+    'Mahendra Singh Dhoni',
+    'RP Singh',
+    'Munaf Patel',
+    'Jasprit bumrah',
+    'Ravichandran Ashwin'
+  ]
+
+  username : string | undefined;
+  userId = 0;
   constructor( private route: ActivatedRoute, private todoService:TodoServiceService) {
     this.route.params.subscribe(data => {this.dataId = data.id;
+
+
       if (this.dataId) this.todoService.getTodo(this.dataId).subscribe(dat => 
         {
-          if(dat) this.dataDet = dat;
+          if(dat) {
+            this.dataDet = dat;
+            this.userId = dat.userId;
+            this.username= this.userNameArray[this.userId];
+          };
         });   
     }
   );
@@ -22,9 +44,12 @@ export class TodoComponent implements OnInit {
   dataDet:any ={};
 
   ngOnInit(): void {
-    console.log(this.dataId);
-   
+    console.log(this.dataId, this.userId, this.username);
+
+    
     console.log(this.dataDet);
+
+
 
   }
 
